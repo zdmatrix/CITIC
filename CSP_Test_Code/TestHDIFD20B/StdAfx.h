3bfe5ebe2bad7d2b49a24829abaecbe84299486e
zdmatrix <zdmatrix@163.com>
2013-12-09 21:44:40 -0800

    TestHDIFD20B project compile sucessed

---
+++ CSP_Test_Code/TestHDIFD20B/StdAfx.h
@@ -0,0 +1,75 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__2AC6C813_065D_4D9C_8952_E1C76E491DF4__INCLUDED_)
+#define AFX_STDAFX_H__2AC6C813_065D_4D9C_8952_E1C76E491DF4__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+
+#include <stdio.h>
+
+#ifndef   _WIN32_WINNT 
+#define   _WIN32_WINNT   0x0400 
+#endif
+
+#include <windows.h>
+#include <wincrypt.h>
+
+#include <malloc.h>
+#include <stdlib.h>
+
+#define MAX_BUF_LEN     512
+#define MAX_APDU_RET_DATA 256
+#define MAX_APDU_ISO_T0   262
+#define MIN_RET_DATA_LEN  4
+
+/********define by hp 070801 in function(GetReaderList)***********
+#define NUMBER_OF_READERS	4
+#define NAME_LENGTH			100
+#define MAX_RESPONSE		2000
+************************************************/
+
+#define EXCUTE_SUC		0x9000
+
+#define CER_RANGE       0xff0c  //The parameters range is invalid 
+#define CER_NOCARD		0xff0f
+#define CER_APDU	    0xff10   //The APDU data format error 
+#define CER_HEXSTR		0xff30	//非法十六进制串，有‘0’～‘9’，‘a’～‘f’，‘A’～‘F’以外的字符，或字符个数不是偶数
+#define CER_RESULT		0xff38	//结果溢出
+#define CER_UNKNOWN     0xffff	//无错误信息给出	
+
+#define CER_PCSC_SCardReleaseContext		0xff60
+#define CER_PCSC_SCardEstablishContext		0xff61
+#define CER_PCSC_SCardListReaders           0xff62
+#define CER_PCSC_SCardListReaders_NONE      0xff63     //No pcsc reader found
+#define CER_PCSC_SCardConnect               0xff64     //
+#define CER_PCSC_ProtocolType               0xff65     //Not T0, T1
+#define CER_PCSC_SCardStatus                0xff66     //Get ATR error
+#define CER_PCSC_SCardReconnect             0xff67     //Reset Card Fail
+#define CER_PCSC_SCardDisconnect            0xff68     //Power off fail
+
+#define CER_PCSC_SCardTransmit              0xff69     //SCardTransmit failed.
+
+#define CER_PCSC_SCardTransmit_Data_LOST    0xff6A     //case 2 data less than 2
+
+#define CER_PCSC_SCardGetAttrib             0xff70     
+#define CER_PCSC_SCardListCards             0xff71     
+#define CER_PCSC_SCardCancel	            0xff72    
+#define CER_PCSC_SCardBeginTransaction      0xff73    
+#define CER_PCSC_SCardEndTransaction        0xff74    
+#define CER_PCSC_SCardGetProviderId         0xff75    
+#define CER_PCSC_SCardGetStatusChange         0xff76 
+
+
+// TODO: reference additional headers your program requires here
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__2AC6C813_065D_4D9C_8952_E1C76E491DF4__INCLUDED_)
---
+++ CSP_Test_Code/TestHDIFD20B/TestHDIFD20B.cpp
@@ -0,0 +1,70 @@
+// TestContainer.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include "HDIFD20B.h"
+
+int main(int argc, char* argv[])
+{
+	HANDLE ContextNo;
+	HANDLE hUKey;
+//	HANDLE devNo;
+
+	unsigned char* srATR;
+	short* srATRLen;
+//	short ivCardSeat;
+	int* ReaderNo;
+	int* ReaderCount;
+
+
+	unsigned short retvalue;
+	unsigned char respData[255];
+	unsigned char apduData[255];
+	short respDataLen = 0;
+
+	ReaderNo = (int*)malloc(sizeof(int));
+	ReaderCount = (int*)malloc(sizeof(int));
+
+	srATR = (unsigned char*)malloc(255 * sizeof(unsigned char));
+	srATRLen = (short*)malloc(sizeof(short));
+/*	
+	apduData[0] = 0x80; //cls
+	apduData[1] = 0xBF; //ins
+	apduData[2] = 0x01; // p1 根据sfi更新
+	apduData[3] = 0x00; // p2 从文件头开始
+	apduData[4] = 0x02; // wCertAddr
+	apduData[5] = 0x38;
+	apduData[6] = 0x32;
+	apduData[7] = 0x00;
+*/
+	apduData[0] = 0x00; //cls
+	apduData[1] = 0x84; //ins
+	apduData[2] = 0x00; // p1 根据sfi更新
+	apduData[3] = 0x00; // p2 从文件头开始
+	apduData[4] = 0x08; // wCertAddr
+	apduData[5] = 0x00;
+	
+	retvalue = HD_ContextInitialize(&ContextNo);
+	if(0x9000 == retvalue){
+		retvalue = HD_GetReaderList(ContextNo, ReaderNo, ReaderCount);
+		if(0x9000 == retvalue){
+			retvalue = HD_OpenPort(21, 9600, 8, &hUKey);
+			if(0x9000 == retvalue){
+				retvalue = HD_ResetCard(hUKey, srATR, srATRLen, 1);
+				if(0x9000 == retvalue){
+					retvalue = HD_ApduT0(hUKey, apduData, 5, respData, &respDataLen, 1);
+					printf("Hello World!\n");
+					getchar();
+				}
+				
+			}
+			
+		}
+		
+	}
+
+	
+	return 0;
+}
+
